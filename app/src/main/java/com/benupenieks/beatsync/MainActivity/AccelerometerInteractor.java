247ee776a2bacce908c41d6fa4b31c629505e069
Ben Upenieks <ben.upenieks@gmail.com>
2017-08-07 14:59:08 -0400

    Now calculates moving average of acceleration vector norms.

--- app/src/main/java/com/benupenieks/beatsync/MainActivity/AccelerometerInteractor.java
+++ app/src/main/java/com/benupenieks/beatsync/MainActivity/AccelerometerInteractor.java
@@ -6,7 +6,11 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import junit.framework.Assert;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Created by Ben on 2017-08-05.
@@ -14,9 +18,23 @@ import java.util.ArrayList;
 
 public class AccelerometerInteractor implements MainContract.Interactor, SensorEventListener {
 
+    private class AccelerometerData {
+        private float mMovingAverage;
+        private long mTimeStamp;
+
+        AccelerometerData(float movingAverage, long timestamp) {
+            mMovingAverage = movingAverage;
+            mTimeStamp = timestamp;
+        }
+
+    }
+
+    private final static int MOVING_AVERAGE_RANGE = 3;
+
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    private ArrayList<Integer[]> mSensorData;
+    private ArrayList<AccelerometerData> mSensorData;
+    private ArrayList<Float> mMovingAverageDataList;
 
     public void init(Context context) {
         mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
@@ -26,6 +44,43 @@ public class AccelerometerInteractor implements MainContract.Interactor, SensorE
     public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     public void onSensorChanged(SensorEvent event) {
+        int dataSize = mMovingAverageDataList.size();
+        if (dataSize >= MOVING_AVERAGE_RANGE) {
+            mMovingAverageDataList.remove(0);
+        }
+        mMovingAverageDataList.add(calcNorm(event.values));
+
+        if (dataSize >= MOVING_AVERAGE_RANGE) {
+            mSensorData.add(
+                    new AccelerometerData(average(mMovingAverageDataList), event.timestamp)
+            );
+        }
+    }
+
+    private float average(ArrayList<Float> data) {
+        float avg = 0;
+        float length = (float) data.size();
+        for (float val : data) {
+            avg += val / length;
+        }
+        return avg;
+    }
+
+    private float calcNorm(float[] data) {
+        // Protects against overflow
+        float max = 0;
+        for (float val : data) {
+            float absVal = Math.abs(val);
+            if (absVal > max) {
+                max = absVal;
+            }
+        }
+        float sum = 0;
+        for (float val : data) {
+            sum += (val / max) * (val / max);
+        }
+
+        return max * (float) Math.sqrt(sum);
     }
 
     public void resume() {}
